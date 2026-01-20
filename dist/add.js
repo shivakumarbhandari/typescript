ef399baffd96ebb6168e53d50514d0c30baf153e
unknown <shivabhandari292@gmail.com>
2026-01-20 11:28:47 +0545

    the value is changed

---
+++ dist/add.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const add = (a, b, c, d) => {
+    return a + b + c + d;
+};
+console.log(add(2, 3, 4, 5));
+//# sourceMappingURL=add.js.map
---
+++ src/add.ts
@@ -0,0 +1,4 @@
+const add =(a : number, b : number, c : number, d : number) =>{
+    return a+b+c+d;
+}
+console.log(add(2, 3, 4, 5));
